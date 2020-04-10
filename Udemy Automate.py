0f656e5207ee74302c893f88cf77e9ca521a8115
Steven-PC <Stevenvdberg15@gmail.com>
2020-04-10 22:27:21 +0200

    Gekloot met f-string en maken van functies van beide Udemy en Python Docs

--- Udemy Automate.py
+++ Udemy Automate.py
@@ -12,10 +12,18 @@ for i, name in enumerate(words):
     print(i+1, name)
 
 for i in range(1, 6):
-    print(f"Jimmy 5 times {i}.")
+    print(f"Jimmy 5 times {i}.", sep=" rgfweroigjn")
 
 pyperclip.copy("SKSJNAMOSIDFJ")
 print(pyperclip.paste())
 
 # Commit  vanuit visual studio code 19:07
 # Inclusief commit vanuit Pycharm? 19:15
+
+
+def exFunction(first_name, second_name):
+    print(f"Hello {first_name}. Oh and better not forget {second_name}. The length of their names is {len(first_name)} letters and {len(second_name)} respectively.")
+
+exFunction("Gert-Jan", "Bram")
+
+print("12", "3", sep="separator")
