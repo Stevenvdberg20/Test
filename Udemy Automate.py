ae04ae1c6a9318a8abdb9a52d7b8cbb6667f2583
Steven-PC <Stevenvdberg15@gmail.com>
2020-04-12 23:19:41 +0200

    Local+global variables, also functions using try-except statements

--- Udemy Automate.py
+++ Udemy Automate.py
@@ -1,4 +1,5 @@
 import pyperclip
+import random
 
 print(bool("1"))
 name = input()
@@ -27,3 +28,50 @@ def exFunction(first_name, second_name):
 exFunction("Gert-Jan", "Bram")
 
 print("12", "3", sep="separator")
+
+# Voor verschil local vs global scope
+def spam():
+    eggs = 42
+    print(eggs)
+
+eggs = "Hello"
+spam()
+print(eggs)
+
+# "While True" zorgt voor een oneindige loop totdat hij "break" tegenkomt. Cool voor inlogshit ofzo.
+def div42by():
+    while True:
+        print("How many cats do you own?")
+        cats = input()
+        try:
+            if int(cats) >  6:
+                print(f"So you have {cats} cats huh. That's cool.")
+            else:
+                print(f"Only {cats} cats, that's nothing.")
+            break
+        except ValueError:
+            print("Please only type numbers.")
+
+# Gebruik van try-except, vooral handig om type van input te valideren.
+def guessNum():
+    print("Hi, what's your name? Let's play guess the number!")
+    player_name = input()
+    print(f"Well hi {player_name}, let's start!")
+    correct_number = random.randint(1,10)
+    guessed_number = 0
+    try_number = 1
+    while guessed_number != correct_number:
+        try:
+            print(f"Try #{try_number}. What is your guess?")
+            guessed_number = int(input())
+            if guessed_number != correct_number:
+                if guessed_number > correct_number:
+                    print("Try a lower number.")
+                else:
+                    print("Aim higher.")
+            try_number = try_number + 1
+        except ValueError:
+            print("Please use numbers only.")
+    print(f"Good job {player_name}, that is correct! You guessed right in {try_number} attempts.")
+
+guessNum()
